206e56822d72326a9cbbd4cf161196c6f341e93f
Stefan8086 <stefan.omidvar@gmail.com>
2023-10-24 17:00:42 +0200

    implement laravel ide helper

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -14,6 +14,31 @@ use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 class VerificationController extends Controller
 {
+     /*
+    |--------------------------------------------------------------------------
+    | Email Verification Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling email verification for any
+    | user that recently registered with the application. Emails may also
+    | be re-sent if the user didn't receive the original email message.
+    |
+    */
+
+    use VerifiesEmails;
+
+    /**
+     * Where to redirect users after verification.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
     public function __construct()
     {
         $this->middleware('auth');
@@ -21,40 +46,4 @@ class VerificationController extends Controller
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
 
-    /**
-     * Display an email verification notice.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function notice(Request $request)
-    {
-        return $request->user()->hasVerifiedEmail() 
-            ? redirect()->route('home') : view('auth.verify-email');
-    }
-
-    /**
-     * User's email verificaiton.
-     *
-     * @param  \Illuminate\Http\EmailVerificationRequest $request
-     * @return \Illuminate\Http\Response
-     */
-    public function verify(EmailVerificationRequest $request)
-    {
-        $request->fulfill();
-        return redirect()->route('home');
-    }
-
-    /**
-     * Resent verificaiton email to user.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function resend(Request $request)
-    {
-        $request->user()->sendEmailVerificationNotification();
-        return back()
-        ->withSuccess('A fresh verification link has been sent to your email address.');
-    }
-
-}
+ }
