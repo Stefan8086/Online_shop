425b99f35132d33dba4927ecf76471ad77afe01b
Stefan8086 <stefan.omidvar@gmail.com>
2023-12-08 15:22:25 +0100

    create migration

--- database/migrations/2023_11_05_170342_create_products_table.php
+++ database/migrations/2023_11_05_170342_create_products_table.php
@@ -14,11 +14,9 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('slug')->unique();
             $table->text('description')->nullable();
             $table->string('image');
-            $table->decimal('sale_price')->nullable();
-            $table->decimal('regular_price' );
+            $table->decimal('price', 8 , 2);
             $table->unsignedInteger('quantity')->default(1);
             $table->float('discount')->nullable();
             $table->enum('status',['active','inactive'])->default('inactive');
