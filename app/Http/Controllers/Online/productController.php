c62682327f6b0c339e26948a99ab769625a77a9d
Stefan8086 <stefan.omidvar@gmail.com>
2023-12-10 15:28:27 +0100

    cheng product  controller

--- app/Http/Controllers/Online/productController.php
+++ app/Http/Controllers/Online/productController.php
@@ -29,102 +29,12 @@ class productController extends Controller
 
     }
 
-    public function cart()
-
-    {
-
-        return view('cart');
-
-    }
-    public function addToCart($id)
-
-    {
-
-        $product = Product::findOrFail($id);
+   
+ }
 
 
 
-        $cart = session()->get('cart', []);
+    
 
+    
 
-
-        if(isset($cart[$id])) {
-
-            $cart[$id]['quantity']++;
-
-        } else {
-
-            $cart[$id] = [
-
-                "name" => $product->name,
-                "quantity" => 1,
-                "price" => $product->price,
-                "image" => $product->image,
-                "description" => $product->description,
-                "status" => $product->status,
-                "discount" => $product->discount,
-
-            ];
-
-        }
-
-
-
-        session()->put('cart', $cart);
-
-        return redirect()->back()->with('success', 'Product added to cart successfully!');
-
-    }
-
-    public function update(Request $request)
-
-    {
-
-        if($request->id && $request->quantity){
-
-            $cart = session()->get('cart');
-
-            $cart[$request->id]["quantity"] = $request->quantity;
-
-            session()->put('cart', $cart);
-
-            session()->flash('success', 'Cart updated successfully');
-
-        }
-
-    }
-
-
-
-    /**
-
-     * Write code on Method
-
-     *
-
-     * @return response()
-
-     */
-
-    public function remove(Request $request)
-
-    {
-
-        if($request->id) {
-
-            $cart = session()->get('cart');
-
-            if(isset($cart[$request->id])) {
-
-                unset($cart[$request->id]);
-
-                session()->put('cart', $cart);
-
-            }
-
-            session()->flash('success', 'Product removed successfully');
-
-        }
-
-    }
-}
