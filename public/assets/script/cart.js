dce71d9abccb1346697c2627bedb75e95f16263d
Stefan8086 <stefan.omidvar@gmail.com>
2023-12-25 15:20:13 +0100

    add to the cart.js

--- public/assets/script/cart.js
+++ public/assets/script/cart.js
@@ -10425,41 +10425,5 @@ $('.dropdown .dropdown-menu li').click(function () {
     }
   });
 
-  €(".update-cart").change(function (e) {
-    e.preventDefault();
 
-    var ele = €(this);
 
-    €.ajax({
-        url: '{{ route('update.cart') }}',
-        method: "patch",
-        data: {
-            _token: '{{ csrf_token() }}',
-            id: ele.parents("tr").attr("data-id"),
-            quantity: ele.parents("tr").find(".quantity").val()
-        },
-        success: function (response) {
-           window.location.reload();
-        }
-    });
-});
-
-€(".remove-from-cart").click(function (e) {
-    e.preventDefault();
-
-    var ele = €(this);
-
-    if(confirm("Are you sure want to remove?")) {
-      €.ajax({
-            url: '{{ route('remove.from.cart') }}',
-            method: "DELETE",
-            data: {
-                _token: '{{ csrf_token() }}',
-                id: ele.parents("tr").attr("data-id")
-            },
-            success: function (response) {
-                window.location.reload();
-            }
-        });
-    }
-});
