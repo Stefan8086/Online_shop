a96bcfa9a0fe57ef35d17680820a0aa8f179ee34
Stefan8086 <stefan.omidvar@gmail.com>
2024-01-06 18:35:40 +0100

    chang migration

---
+++ database/migrations/2023_12_28_161802_create_settings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('key');
+            $table->string('display_name');
+            $table->text('value')->nullable();
+            $table->text('details')->nullable();
+            $table->string('type');
+            $table->string('group');
+            $table->integer('order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
